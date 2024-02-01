abe00fc53a4245350a3d4a06789ab730d4d2beb5
Cristianpl08 <146230241+Cristianpl08@users.noreply.github.com>
2024-01-31 20:38:36 -0500

    Add files via upload

---
+++ matrices.py
@@ -0,0 +1,8 @@
+import numpy as np
+a=np.array([2,3,4])
+b=np.array([[1,4,5],[1,2,4],[2,5,6]])
+print(a)
+print(b)
+t=np.array([0,1,2,3,4,5,6,7,8,9,10,11,12])
+for i in range (0,12):
+    print("valor anterior: ",t[i+1])
